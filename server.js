4616b941cd27f8291c151fe5002e2db1a930c640
Ayush Jain <ayushjn62@gmail.com>
2023-03-06 11:29:12 +0530

    fixes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path")
 const express = require('express')
 const app = express()
 const https = require('https')
-const server = require('http').Server(app)
-// const server = https.createServer({
-//   key : fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-//   cert : fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-// }, app)
+// const server = require('http').Server(app)
+const server = https.createServer({
+  key : fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+  cert : fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
